b3cc591006692e7c21326669f5da2cf6025801a9
Your Name <you@example.com>
2019-11-23 14:57:24 +0800

    wogaide

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+gogogooijkhjggh
+wwwwww
+import (
+        "github.com/micro/go-log"
+	"net/http"
+
+        "github.com/micro/go-web"
+        "sssv/IhomeWeb/handler"
+)
+
+func main() {
+	// create new web service
+        service := web.NewService(
+                web.Name("go.micro.web.IhomeWeb"),
+                web.Version("latest"),
+        )
+
+	// initialise service
+        if err := service.Init(); err != nil {
+                log.Fatal(err)
+        }
+
+	// register html handler
+	service.Handle("/", http.FileServer(http.Dir("html")))
+
+	// register call handler
+	service.HandleFunc("/example/call", handler.ExampleCall)
+
+	// run service
+        if err := service.Run(); err != nil {
+                log.Fatal(err)
+        }
+}
